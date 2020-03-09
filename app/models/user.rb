6ada898d9506938c4677d415fbc952052d6a6cfd
w-tensyo <dr.tensyo@gmail.com>
2020-03-09 11:42:41 +0900

    create-message-routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
+  has_many :group_users
 end
