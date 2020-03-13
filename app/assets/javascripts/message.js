934af8de7ebef9d249a1f7cdc4eadad4f69a17ca
w-tensyo <dr.tensyo@gmail.com>
2020-03-14 00:08:43 +0900

    fix secrets.yml

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -83,7 +83,7 @@ $(function(){
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.msg:last').data("message-id");
-    $.ajax({
+    $.ajax({ 
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
